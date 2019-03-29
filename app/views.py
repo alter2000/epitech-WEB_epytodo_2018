29abb2f0cfe9586f0cfb9e8e54b853230d34d422
Jor Bashllari <altnate2000@gmail.com>
2019-03-29 16:36:36 +0100

    add globals and views

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, jsonify
 import json
 
 from app import app
+import controller as con
 
 
 # index route, shows index.html view
@@ -10,26 +11,53 @@ def index():
     return render_template('index.html')
 
 
-# endpoint for storing todo item
-@app.route('/add-todo', methods=['POST'])
-def addTodo():
+@app.route('/register', methods=['POST'])
+def user_register(name):
+    return render_template('index.html', username=name)
+
+
+@app.route('/signin', methods=['POST'])
+def user_sign_in(name):
+    return render_template('index.html', username=name)
+
+
+@app.route('/signout', methods=['POST'])
+def user_sign_out(name):
+    return render_template('index.html', username=name)
+
+
+@app.route('/<name>', methods=['GET'])
+def user_show_user(name):
+    return render_template('index.html', username=name)
+
+
+@app.route('/<name>/task', methods=['GET'])
+def user_show_tasks(name):
+    return render_template('index.html', username=name)
+
+
+@app.route('/<name>/task/<task>', methods=['GET', 'POST'])
+def user_single(name, task):
+    if request.method == 'GET':
+        return jsonify({
+                'id': task,
+                'completed': json.loads(request.data).get('completed', 0)
+                })
+    elif request.method == 'POST':
+        if con.login_valid(request.form['username'],
+                           request.form['password']):
+            return render_template('index.html', username=name)
+
+
+@app.route('/<name>/task/add', methods=['POST'])
+def user_add(name):
     data = json.loads(request.data)  # load JSON data from request
     return jsonify(data)
 
 
-# endpoint for deleting todo item
-@app.route('/remove-todo/<item_id>')
-def removeTodo(item_id):
+@app.route('/<name>/task/del/<task>', methods=['POST'])
+def user_del(name, task):
     return jsonify({
-                    'id': item_id,
+                    'uid': name,
+                    'tid': task,
                    })
-
-
-# endpoint for updating todo item
-@app.route('/update-todo/<item_id>', methods=['POST'])
-def updateTodo(item_id):
-    data = {
-            'id': item_id,
-            'completed': json.loads(request.data).get('completed', 0)
-            }
-    return jsonify(data)
