40dd1a5f2e625b6ba2a27b052b12502343be3f23
Jor Bashllari <altnate2000@gmail.com>
2019-05-08 15:27:05 +0200

    modify schema to use 2 tables

--- epytodo.sql
+++ epytodo.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS epytodo;
 USE epytodo;
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user
+CREATE TABLE IF NOT EXISTS user
+/* DROP TABLE IF EXISTS user;
+   CREATE TABLE user */
 (
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) NOT NULL,
@@ -10,24 +11,22 @@ CREATE TABLE user
 	u_mail VARCHAR(255),
 	u_tasks INT,
 	u_tasks_completed INT
-	UNIQUE_KEY(user_id, username)
 ) DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS task;
-CREATE TABLE task
+CREATE TABLE IF NOT EXISTS task
+/* DROP TABLE IF EXISTS task;
+   CREATE TABLE task */
 (
 	task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(255) NOT NULL,
 	begin TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	end TIMESTAMP DEFAULT 0,
 	status INT DEFAULT 0,
-	t_description VARCHAR(2048)
-);
+	t_description VARCHAR(2048),
 
-DROP TABLE IF EXISTS user_has_task;
-CREATE TABLE user_has_task
-(
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	fk_user_id INT NOT NULL FOREIGN KEY user_has_task(fk_user_id) REFERENCES task(task_id),
-	fk_task_id INT NOT NULL FOREIGN KEY user_has_task(fk_task_id) REFERENCES user(user_id)
+	user_own INT NOT NULL,
+	CONSTRAINT `t_fk_user`
+		FOREIGN KEY (`user_own`) REFERENCES `user` (`user_id`)
+			ON DELETE CASCADE
+			ON UPDATE RESTRICT
 );
